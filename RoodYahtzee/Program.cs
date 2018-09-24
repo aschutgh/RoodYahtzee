e63c313282f4ba425515f0eb98f9a87dd566e7a8
Arthur Schut <aschutgh@gmail.com>
2018-09-24 13:38:20 +0200

    renamed form1.cs

--- RoodYahtzee/Program.cs
+++ RoodYahtzee/Program.cs
@@ -16,7 +16,7 @@ namespace RoodYahtzee
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new YahtzeePrimaryForm());
         }
     }
 }
--- RoodYahtzee/YahtzeePrimaryForm.cs
+++ RoodYahtzee/YahtzeePrimaryForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RoodYahtzee
 {
-    public partial class Form1 : Form
+    public partial class YahtzeePrimaryForm : Form
     {
-        public Form1()
+        public YahtzeePrimaryForm()
         {
             InitializeComponent();
         }
